ab8f95928626bb0e5d04213419ffe28396b1f837
Elina Shelest <evengene@gmail.com>
2024-05-22 14:33:08 -0600

    wip

--- src/server/atlasClient.ts
+++ src/server/atlasClient.ts
@@ -1,31 +1,35 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import { Db, MongoClient, ServerApiVersion } from 'mongodb';
 import { MONGO_DB_CONNECTION_STRING } from '../constants/shared';
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(MONGO_DB_CONNECTION_STRING, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// const client = new MongoClient(MONGO_DB_CONNECTION_STRING, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+let db: Db | null = null;
 
 
 // TODO: Refactor other routes to use this function
 const getDb = async () => {
-
-  let connection;
+  if (db) {
+    return db;
+  }
 
   try {
-    connection = await client.connect();
+    const client = await MongoClient.connect(MONGO_DB_CONNECTION_STRING);
+    db = client.db();
   } catch (error) {
     console.error(error);
     throw error;
   }
 
-  const database = connection.db('markdown');
+  // const database = connection.db('markdown');
 
-  return database;
+  return db;
 
 }
 
