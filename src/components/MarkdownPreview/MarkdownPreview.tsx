8ff388b96ce59f7ebdac72ed3fdd4b42e8b7cf86
Elina Shelest <evengene@gmail.com>
2024-06-18 12:19:34 -0600

    icon styles

--- src/components/MarkdownPreview/MarkdownPreview.tsx
+++ src/components/MarkdownPreview/MarkdownPreview.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import type { FC } from 'react';
 import { marked } from 'marked';
-import { RootState } from "../../store/store";
+import { RootState, useDispatch } from "../../store/store";
 import { useSelector } from "react-redux";
 import commonStyles from './MarkdownPreview.module.css';
 import styles from '../common.module.css';
+import Image from "next/image";
+import previewIcon from "../../assets/icon-show-preview.svg";
+import { togglePagePreview } from "../../store/markdownSlice";
 
 interface MarkdownPreviewProps {}
 
 export const MarkdownPreview: FC<MarkdownPreviewProps> = ( props ) => {
 
+  const dispatch = useDispatch();
+
   const { theme } = useSelector(( state: RootState ) => state.markdown);
   const markdown = useSelector(( state: RootState ) => state.markdown.content);
   const html = (marked as any)(markdown);
 
+  const onPreviewClick = () => {
+    dispatch(togglePagePreview());
+  }
 
   return (
     <div className={styles.container}>
       <div className={`${styles.title} ${theme === 'light' ? styles.titleLight : styles.title}`}>
         <h1>Preview</h1>
+        <Image src={previewIcon} alt="preview" className={styles.icon} onClick={onPreviewClick} />
       </div>
       <div
         className={commonStyles.textPreview}
--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -2,21 +2,13 @@ import React from 'react';
 import styles from './Wrapper.module.css';
 import { useAppSelector, useDispatch } from '../../store/store';
 import { EMPTY_STRING } from '../../constants/shared';
-import Image from "next/image";
-import previewIcon from '../../assets/icon-show-preview.svg';
-import { togglePagePreview } from "../../store/markdownSlice";
 
 export const Wrapper = (props: { children: React.ReactNode }) => {
-  const dispatch = useDispatch();
 
   const { sidebarOpen } = useAppSelector(( state ) => state.sidebar);
 
-  const onPreviewClick = () => {
-    dispatch(togglePagePreview());
-  }
   return (
     <div className={`${styles.wrapper} ${sidebarOpen ? styles.sidebarOpen : EMPTY_STRING}`}>
-      <Image src={previewIcon} alt="preview" className={styles.previewIcon} onClick={onPreviewClick} />
       {props.children}
     </div>
   );
