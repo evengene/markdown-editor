8d51e2f16e7e5e997a6605aa4c5c80bdfa029100
elina <evengene@gmail.com>
2024-06-18 19:50:14 -0600

    move env vars

--- src/constants/shared.ts
+++ src/constants/shared.ts
@@ -1,6 +1,6 @@
 // import dotenv from 'dotenv';
 //
 // dotenv.config();
-export const MONGO_DB_CONNECTION_STRING= `mongodb+srv://elinafrontenddev:PVPf3XHXtE2rtw46@mydb.azyhahy.mongodb.net/?retryWrites=true&w=majority&appName=MyDb`;
+export const MONGO_DB_CONNECTION_STRING= `mongodb+srv://elinafrontenddev:${process.env.MONGO_DB_PASSWORD}@mydb.azyhahy.mongodb.net/?retryWrites=true&w=majority&appName=MyDb`;
 export const EMPTY_STRING = '';
 export const DEFAULT_DOCUMENT_NAME = 'Untitled Document';
